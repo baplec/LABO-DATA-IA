50779dea33b90aa1124bf069cf61e9d975713afc
matheodrd <matheodurand14@gmail.com>
2024-11-14 18:43:37 +0100

    add post prompt endpoint

--- backend/labo_data_ia/main.py
+++ backend/labo_data_ia/main.py
@@ -9,5 +9,9 @@ app = FastAPI()
 async def root() -> Dict[str, str]:
     return {"message": "hello :)"}
 
+@app.post("/prompt")
+async def post_prompt(prompt: str) -> Dict[str, str]:
+    return {"message": f"The following prompt was submitted: {prompt}"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
